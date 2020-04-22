434790c0722808709a8f25da3fa8313065b907fb
JHOSEP GRANDA <jhosep.granda@educatic.com.co>
2020-04-21 19:13:33 -0500

    initial

--- src/app/image.service.spec.ts
+++ src/app/image.service.spec.ts
@@ -6,11 +6,38 @@ describe('ImageService', () => {
   let service: ImageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ImageService);
+    service = new ImageService();
   });
 
-  it('should be created', () => {
+  it('Deberia crear el servicio', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('#getImage', () =>  {
+    it('deberia retornar todas las imagenes', () => {
+      let imagenes = service.getImages();
+      expect(imagenes.length).toEqual(5);
+    })
+  });
+
+  describe('#getImage(id)', () =>  {
+    it('deberia retornar indefinido cuando se envia indefinido ', () => {
+      let id;
+      let imagen = service.getImage(id);
+      expect(imagen).toBeUndefined();
+    });
+
+    it('deberia retornar indefinido cuando se envia como id no existente ', () => {
+      let id=20;
+      let imagen = service.getImage(id);
+      expect(imagen).toBeUndefined();
+    })
+
+    it('deberia retornar un perro cuando se envia como id el 1 ', () => {
+      let id=1;
+      let imagen = service.getImage(id);
+      expect(imagen.brand).toEqual("perro");
+    })
+
   });
 });
